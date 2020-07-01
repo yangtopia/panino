e143c24700ccaf1d0146d751e42d208c88deb205
yangtopia <yangtopia@gmail.com>
2020-07-01 17:06:44 +0900

    FIX

--- android/app/src/main/kotlin/com/example/panino/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.panino
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/kr/co/sandylabs/panino/MainActivity.kt
+++ android/app/src/main/kotlin/kr/co/sandylabs/panino/MainActivity.kt
@@ -1,9 +1,24 @@
 package kr.co.sandylabs.panino
 
-import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-  companion object {
-    const val CHANNEL = "kr.co.sandylabs/value"
-  }
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "kr.co.sandylabs/value"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        GeneratedPluginRegistrant.registerWith(FlutterEngine(this))
+        MethodChannel(flutterView, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getValue") {
+                result.success("성공")
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
 }
