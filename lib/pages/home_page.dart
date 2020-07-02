79931e4614696832c939f9c4d0af2071ea8f0ce6
yangtopia <yangtopia@gmail.com>
2020-07-02 22:48:40 +0900

    streambuilder로 페이지 이동 구현

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,40 +1,42 @@
 import 'package:flutter/material.dart';
+import '../bloc/page_index_bloc.dart';
 
-class MainPage extends StatefulWidget {
-  @override
-  _MainPageState createState() => _MainPageState();
-}
+final List<Widget> _pages = [HomePage(), null, null, null, null];
+PageIndexBloc pageIndexBloc = PageIndexBloc();
 
-class _MainPageState extends State<MainPage> {
-  int _currentPageIndex = 0;
-  final List<Widget> _pages = [HomePage(), null, null, null, null];
-
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_currentPageIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('홈')),
-          BottomNavigationBarItem(icon: Icon(Icons.movie), title: Text('동영상')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite), title: Text('좋아요')),
-          BottomNavigationBarItem(icon: Icon(Icons.list), title: Text('주문내역')),
-          BottomNavigationBarItem(icon: Icon(Icons.info), title: Text('MY')),
-        ],
-        fixedColor: Colors.blue,
-        currentIndex: _currentPageIndex,
-        onTap: _onTab,
-        unselectedItemColor: Colors.grey,
-        showUnselectedLabels: true,
-      ),
-    );
+    return StreamBuilder<int>(
+        stream: pageIndexBloc.pageIndexStream$,
+        builder: (context, snapshot) {
+          return Scaffold(
+            body: _pages[snapshot.data],
+            bottomNavigationBar: BottomNavigationBar(
+              items: [
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.home), title: Text('홈')),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.movie), title: Text('동영상')),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.favorite), title: Text('좋아요')),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.list), title: Text('주문내역')),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.info), title: Text('MY')),
+              ],
+              fixedColor: Colors.blue,
+              currentIndex: snapshot.data,
+              onTap: _onTab,
+              unselectedItemColor: Colors.grey,
+              showUnselectedLabels: true,
+            ),
+          );
+        });
   }
 
   void _onTab(int selectedPageIndex) {
-    setState(() {
-      _currentPageIndex = selectedPageIndex;
-    });
+    pageIndexBloc.changePageIndex(selectedPageIndex);
   }
 }
 
